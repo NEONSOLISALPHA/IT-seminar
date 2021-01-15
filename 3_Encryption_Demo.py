da91efbf1c5f6bdbe11ed314c5083471293ad2da
NEONSOLISALPHA <adithyadev101@gmail.com>
2021-01-15 14:11:37 +0530

    moved AKCDemo inside Encryption Demo

--- 3_Encryption_Demo.py
+++ 3_Encryption_Demo.py
@@ -128,3 +128,162 @@ class SKCDemo(Scene):
                 ]
             ]
         )
+
+
+class AKCDemo(Scene):
+    def construct(self):
+        # scales for all the componenents
+        file_scale = 0.8
+        key_demo_scale = 1.9
+        key_scale = 0.96
+        cleartext_scale = 0.8
+        ciphertext_scale = 0.63
+        process_scale = 1.2
+        key_down_pos = 2.9
+        key_retrieve_length = 1.7
+
+        title = TextMobject("Symmetric Key Cryptography").scale(1.4)
+        title_transformed = TextMobject("Asymmetric Key Cryptography")
+        title_transformed.scale(1.4)
+        self.play(FadeInFromDown(title))
+        self.play(Transform(title, title_transformed))
+
+        title.generate_target()
+        title.target.scale(0.7)
+        title.target.to_edge(UP, buff=SMALL_BUFF * 2.4)
+
+        pk = ImageMobject(r"animations/assets/pk.png").scale(key_demo_scale)
+        sk = ImageMobject(r"animations/assets/sk.png").scale(key_demo_scale)
+        keys = Group(pk, sk).arrange(RIGHT, buff=4)
+        pk_label = TextMobject("public key").next_to(
+            keys[0], DOWN, buff=SMALL_BUFF + 0.2
+        )
+        sk_label = TextMobject("private key").next_to(
+            keys[1], DOWN, buff=SMALL_BUFF + 0.2
+        )
+        labels = [pk_label, sk_label]
+        self.play(MoveToTarget(title), *[FadeInFromDown(i) for i in keys])
+        self.play(*[FadeInFromDown(i) for i in labels])
+
+        # clear_text file definition and moving it to the left edge
+        file_1 = ImageMobject(r"animations/assets/file_icon.png").scale(
+            file_scale,
+        )
+        file_2 = file_1.copy()
+        file_1.to_edge(LEFT, buff=LARGE_BUFF)
+
+        # "Hello" follows around file_1 using file_1.add_updater()
+        file_1_label = (
+            TextMobject("Hello")
+            .move_to(file_1.get_center())
+            .scale(cleartext_scale)  # noqa: E501
+        )
+        file_1_label.add_updater(lambda m: m.move_to(file_1.get_center()))
+
+        """
+        ciphertext file label  and moving it
+        to the center of decryption image
+        """
+        file_2_label = TextMobject("IsQ@pq").scale(ciphertext_scale)
+        file_2_label.move_to(file_2.get_center())
+
+        file_2_label.add_updater(lambda m: m.move_to(file_2.get_center()))
+        encryption = (
+            ImageMobject(r"animations/assets/PGP_Encryption.png")
+            .shift(LEFT * 2.6)
+            .scale(process_scale)
+        )
+        decryption = (
+            ImageMobject(r"animations/assets/PGP_Decryption.png")
+            .shift(RIGHT * 2.6)
+            .scale(process_scale)
+        )
+
+        file_2.move_to(encryption.get_center())
+
+        """
+        HACK had to create a duplicate cause file_2
+        wouldn't go back to full opacity so, yeah...
+        """
+        dupli = file_2.copy().set_opacity(0)
+
+        """
+        declaring both keys and moving them DOWN below the center of
+        processes
+        """
+        pk = ImageMobject(r"animations/assets/pk.png").scale(key_scale)
+        pk.move_to(encryption.get_center() + DOWN * key_down_pos)
+        sk = ImageMobject(r"animations/assets/sk.png").scale(key_scale)
+        sk.move_to(decryption.get_center() + DOWN * key_down_pos)
+
+        self.play(
+            *[FadeOutAndShiftDown(i) for i in labels],
+            ReplacementTransform(keys[0], pk),
+            ReplacementTransform(keys[1], sk),
+            *[
+                FadeInFromDown(i)
+                for i in [
+                    file_1,
+                    dupli,
+                    file_2_label.set_opacity(0),
+                    encryption,
+                    decryption,
+                    file_1_label,
+                ]
+            ],
+        )
+
+        # bringing mobjects to farthest back layer
+        self.bring_to_back(file_2, file_1, file_1_label, file_2_label, pk, sk)
+
+        """
+        HACK file_2 didn't want to change opacity back
+        to 255 so ended up having to do this...
+        """
+        ReplacementTransform(dupli, file_2)
+        file_2_label.set_opacity(255)
+        self.play(file_1.move_to, encryption.get_center())
+
+        def move_key(key):
+            key.save_state()
+            self.play(
+                key.shift,
+                UP * key_retrieve_length,
+                rate_func=lambda t: exponential_decay(t, 0.22),
+            )
+            self.play(Restore(key))
+
+        move_key(pk)
+
+        self.play(file_2.move_to, ORIGIN)
+        self.play(file_2.move_to, decryption.get_center())
+        self.remove(file_2, file_2_label)
+
+        move_key(sk)
+
+        file_1.move_to(decryption.get_center())
+        self.play(ApplyMethod(file_1.to_edge, RIGHT, {"buff": LARGE_BUFF}))
+
+        """
+        HACK file_2_label refuses to disappear,
+        so i had to make a fake one and transform
+        """
+        label_duplicate = TextMobject("Hello").scale(cleartext_scale)
+        label_duplicate.move_to(file_1.get_center())
+        ReplacementTransform(file_1_label, label_duplicate)
+        self.remove(file_1_label)
+
+        self.play(
+            *[
+                FadeOutAndShiftDown(i)
+                for i in [
+                    pk,
+                    sk,
+                    title,
+                    file_1,
+                    label_duplicate,
+                    encryption,
+                    decryption,
+                ]
+            ]
+        )
